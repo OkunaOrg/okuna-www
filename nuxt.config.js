28be2c1c57a87f379b33e74002560af37b144b51
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-10 17:18:15 +0200

    🔥 clean up nuxt starter project files

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,21 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'openbook-org-www',
+    title: 'The Openbook Organization',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The Openbook Organization website' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'The Openbook Organization website'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:400,600,700,800|Roboto:400,500,700'}
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#3B8070'},
   /*
   ** Build configuration
   */
@@ -24,7 +25,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -33,6 +34,15 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+
+      config.module.rules.push( {
+        test: /\.exec\.js$/,
+        use: [ 'script-loader' ]
+      })
+
     }
-  }
+  },
+  css: [
+    '@/assets/styles/index.scss'
+  ]
 }
