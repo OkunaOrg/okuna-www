00f7fdc77042d743834e4bfa2b7e049d2606587c
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-11 09:45:54 +0200

    ✨ add nuxt-i18n

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        openbook-org-www
+        {{ $t("home.title") }}
       </h1>
       <h2 class="subtitle">
         The Openbook Organization website
