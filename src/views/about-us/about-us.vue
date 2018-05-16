19683f86a2dd5426be61cd120d5f559c9debe185
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-16 22:57:57 +0200

    🚧 add about us hero desktop version

--- src/views/about-us/about-us.vue
+++ src/views/about-us/about-us.vue
@@ -1,24 +1,32 @@
 <template>
     <div>
-        <section class="hero is-fullheight hero---about-us">
+        <section class="hero is-fullheight hero--about-us">
             <div class="hero-body">
                 <div class="container is-fluid">
                     <div class="columns is-centered is-vcentered">
                         <div class="column">
-                            <h1 class="title trust-text has-text-centered">
-                                We want to build a social network we can all trust.
-                            </h1>
-                            <h2 class="subtitle has-text-right">
-                                <i>- Joel Hernandez</i><br/>
-                                <span class="is-size-7">Founder of Openbook</span>
-                            </h2>
+                            <div class="columns is-centered">
+                                <div class="column is-narrow">
+                                    <h1 class="subtitle is-size-3 has-text-grey-light">
+                                        About us
+                                    </h1>
+                                    <h2 class="title is-size-2 trust-text">
+                                        We want to build a social network we all can <span class="has-text-primary">trust</span>.
+                                    </h2>
+                                </div>
+                            </div>
                         </div>
                         <div class="column">
-                            <div class="columns is-centered is-vcentered is-mobile">
+                            <div class="columns is-centered is-vcentered is-mobile padding-bottom-2x">
                                 <div class="column is-narrow">
-                                    <figure class="image is-semi-rounded is-shadowed trust-gif">
-                                        <img src="https://i.giphy.com/media/Jz75B6BpVG728/giphy.webp">
+                                    <figure class="image">
+                                        <img class="is-rounded trust-gif" src="https://orig00.deviantart.net/b64a/f/2015/196/4/6/earth_3_0__animated__by_shaddow24-d91hqbn.gif">
                                     </figure>
+                                </div>
+                            </div>
+                            <div class="columns">
+                                <div class="column has-text-centered">
+                                    <p>The place we all call home.</p>
                                 </div>
                             </div>
                         </div>
@@ -28,9 +36,11 @@
         </section>
         <section class="section">
             <div class="container">
-                <div class="columns">
-                    <div class="column has-text-centered padding-bottom-3x">
-                        <h2 class="is-size-3">Why are we doing this?</h2>
+                <div class="columns is-centered">
+                    <div class="column has-text-centered intro-text">
+                        <h3 class="is-size-4">
+                            In the year 2018, data scandal after data scandal, tracking tools improving their efficiency thanks to the development of artificial intelligence,
+                        </h3>
                     </div>
                 </div>
 
@@ -42,12 +52,21 @@
 
 <style lang="scss">
 
+    .hero--about-us{
+        border-bottom: solid 1px #fafafa;
+    }
+
     .trust-text{
-        max-width: 400px;
+        max-width:500px;
     }
 
     .trust-gif{
-        max-width: 600px;
+        max-width: 300px;
         height: auto;
     }
+
+    .intro-text{
+        max-width: 600px;
+    }
+
 </style>
