333bcdc9004794800728bf2a46e66699ad2c4182
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-16 18:47:50 +0200

    🚧 add about us hero skeleton

--- src/views/about-us/about-us.vue
+++ src/views/about-us/about-us.vue
@@ -1,7 +1,53 @@
 <template>
     <div>
-        <h1>
-            About us!
-        </h1>
+        <section class="hero is-fullheight hero---about-us">
+            <div class="hero-body">
+                <div class="container is-fluid">
+                    <div class="columns is-centered is-vcentered">
+                        <div class="column">
+                            <h1 class="title trust-text has-text-centered">
+                                We want to build a social network we can all trust.
+                            </h1>
+                            <h2 class="subtitle has-text-right">
+                                <i>- Joel Hernandez</i><br/>
+                                <span class="is-size-7">Founder of Openbook</span>
+                            </h2>
+                        </div>
+                        <div class="column">
+                            <div class="columns is-centered is-vcentered is-mobile">
+                                <div class="column is-narrow">
+                                    <figure class="image is-semi-rounded is-shadowed trust-gif">
+                                        <img src="https://i.giphy.com/media/Jz75B6BpVG728/giphy.webp">
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column has-text-centered padding-bottom-3x">
+                        <h2 class="is-size-3">Why are we doing this?</h2>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     </div>
 </template>
+
+
+<style lang="scss">
+
+    .trust-text{
+        max-width: 400px;
+    }
+
+    .trust-gif{
+        max-width: 600px;
+        height: auto;
+    }
+</style>
