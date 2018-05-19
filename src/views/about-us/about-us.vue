813c947d9e772238e1698bd08d8b88a29b4cb1dd
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-19 18:08:06 +0200

    ✨ make about us header mobile friendly

--- src/views/about-us/about-us.vue
+++ src/views/about-us/about-us.vue
@@ -4,23 +4,23 @@
             <div class="hero-body">
                 <div class="container is-fluid">
                     <div class="columns is-centered is-vcentered">
-                        <div class="column">
+                        <div class="column about-us-header-text">
                             <div class="columns is-centered">
                                 <div class="column is-narrow">
-                                    <h1 class="subtitle is-size-3 has-text-grey-light">
+                                    <h1 class="subtitle is-size-3 has-text-grey-light has-text-centered-mobile">
                                         About us
                                     </h1>
-                                    <h2 class="title is-size-2 trust-text">
+                                    <h2 class="title is-size-2 trust-text has-text-centered-mobile">
                                         We want to build a tool for <span class="has-text-primary">good</span>.
                                     </h2>
                                 </div>
                             </div>
                         </div>
-                        <div class="column">
+                        <div class="column about-us-header-gif">
                             <div class="columns is-centered is-vcentered is-mobile padding-bottom-2x">
                                 <div class="column is-narrow">
-                                    <figure class="image">
-                                        <img class="is-rounded trust-gif" src="https://orig00.deviantart.net/b64a/f/2015/196/4/6/earth_3_0__animated__by_shaddow24-d91hqbn.gif">
+                                    <figure class="image trust-gif">
+                                        <img class="is-rounded is-square trust-gif__img" src="https://orig00.deviantart.net/b64a/f/2015/196/4/6/earth_3_0__animated__by_shaddow24-d91hqbn.gif">
                                     </figure>
                                 </div>
                             </div>
@@ -43,15 +43,15 @@
                         </p>
                         <br/>
                         <p>
-                            Meant to connect us, they divide us by feeding us information that reinforce our personal beliefs and discourage dialogue.
+                            Meant to connect us, they divide us by feeding us information that reinforce our personal beliefs, discouraging dialogue.
                         </p>
                         <br/>
                         <p>
-                            Meant to help us share our treasured moments with our loved ones,  they share our information with third parties following political, economical or disruptive agendas.
+                            Meant to help us share our treasured moments with our loved ones,  they share our information with third parties, working under economical, political or disruptive agendas.
                         </p>
                         <br/>
                         <p>
-                            Meant to be a tool for good, they contribute to the development of a global mass surveillance network, threatening all civil liberties.
+                            Meant to be a tool for good, they contribute to the development of a global mass surveillance network, threatening all of our civil liberties.
                         </p>
                         <br/>
                         <p>
@@ -97,6 +97,24 @@
                 <ob-team-members/>
             </div>
         </section>
+        <section class="hero is-primary is-bold">
+            <div class="hero-body has-text-centered">
+                <div class="container">
+                    <div class="columns is-centered is-vcentered">
+                        <div class="column">
+                            <h3 class="title is-size-4">
+                                Want to work with us?
+                            </h3>
+                        </div>
+                        <div class="column">
+                            <a href="#" class="button is-white is-outlined is-rounded is-medium">
+                                Visit our jobs page
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -106,13 +124,34 @@
         border-bottom: solid 1px #d4d4d4;
     }
 
+    .about-us-header-text{
+        @media(max-width: 768px){
+            margin-top: 1rem;
+        }
+    }
+
+    .about-us-header-gif{
+        @media(max-width: 768px){
+            margin-top: 1rem;
+        }
+    }
+
     .trust-text{
-        max-width:400px;
+        @media (min-width: 768px){
+            max-width:400px;
+        }
     }
 
     .trust-gif{
-        max-width: 300px;
-        height: auto;
+        position: relative;
+        display: flex !important;
+        align-items: center;
+        justify-content: center;
+
+        &__img{
+            max-width: 50%;
+            height: auto;
+        }
     }
 
     .intro-text{
