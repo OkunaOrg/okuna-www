9f3544804c2b3a5aa80b0bf5a0af496a28679f4a
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-20 14:45:09 +0200

    ✨ add faq with questions from old site

---
+++ src/views/faq/components/pets.vue
@@ -0,0 +1,386 @@
+<template>
+    <div class="columns">
+        <div class="pets-right column">
+            <div class="dog">
+                <div class="ears2"></div>
+                <div class="head2">
+                    <div class="eyes2"></div>
+                    <div class="nose2"></div>
+                </div>
+                <div class="body2">
+                    <div class="left-paw2"></div>
+                    <div class="right-paw2"></div>
+                </div>
+                <div class="tail2"></div>
+                <div class="ORlaptop">
+                    <div class="ORscreen"></div>
+                    <div class="ORkeyboard"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss">
+    // Pet courtesy of the amazing Clara Song https://codepen.io/clarawrsong/
+    // https://codepen.io/clarawrsong/pen/JMMdOz
+
+    .pets-right {
+        position: relative;
+        height: 100%;
+        width: 50%;
+    }
+    .dog {
+        height: 182px;
+        position: relative;
+        top: 50%;
+        padding-left: 85px;
+    }
+    .dog .ears2 {
+        height: 0;
+        width: 0;
+        position: relative;
+        left: 30px;
+        border-bottom: 27px solid #F07E42;
+        border-left: 23px solid transparent;
+        border-right: 10px solid transparent;
+    }
+    .dog .ears2::before {
+        display: block;
+        content: "";
+        height: 0;
+        width: 0;
+        position: relative;
+        left: 33px;
+        border-bottom: 27px solid #F07E42;
+        border-left: 10px solid transparent;
+        border-right: 23px solid transparent;
+    }
+    .dog .head2 {
+        height: 74px;
+        width: 140px;
+        position: relative;
+        z-index: 2;
+        box-shadow: 8px 0 0 #F07E42;
+        border-radius: 35px;
+        background: #FFA852;
+    }
+    .dog .head2 .eyes2 {
+        height: 12px;
+        width: 12px;
+        position: relative;
+        top: 37px;
+        left: 50px;
+        border-radius: 100%;
+        animation: 9s dogRead infinite;
+        background: black;
+    }
+    .dog .head2 .eyes2::before {
+        display: block;
+        content: "";
+        height: 12px;
+        width: 12px;
+        position: relative;
+        left: 18px;
+        border-radius: 100%;
+        background: black;
+    }
+    .dog .head2 .nose2 {
+        height: 30px;
+        width: 45px;
+        position: relative;
+        top: 40px;
+        left: 37px;
+        border-radius: 20px;
+        background: #FBF1D8;
+    }
+    .dog .head2 .nose2::before {
+        display: block;
+        content: "";
+        height: 0;
+        width: 0;
+        position: relative;
+        top: 3px;
+        left: 9px;
+        border-radius: 10px;
+        border-top: 10px solid black;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+    }
+    .dog .body2 {
+        height: 110px;
+        width: 150px;
+        position: relative;
+        top: -30px;
+        z-index: 1;
+        border-radius: 55px;
+        background: #FFA852;
+    }
+    .dog .body2 .pets-left-paw2 {
+        height: 25px;
+        width: 37px;
+        position: relative;
+        top: 70px;
+        left: 15px;
+        border-radius: 12px;
+        animation: 9s dogLeftType infinite;
+        background: #FBF1D8;
+    }
+    .dog .body2 .pets-right-paw2 {
+        height: 25px;
+        width: 37px;
+        position: relative;
+        top: 45px;
+        left: 60px;
+        border-radius: 12px;
+        animation: 9s dogRightType infinite;
+        background: #FBF1D8;
+    }
+    .dog .tail2 {
+        height: 34px;
+        width: 70px;
+        position: relative;
+        top: -64px;
+        left: 100px;
+        z-index: 0;
+        border-radius: 0 17px 17px 0;
+        background: #F07E42;
+    }
+    .dog .ORlaptop {
+        position: relative;
+        top: -161px;
+        left: -103px;
+        z-index: 2;
+    }
+    .dog .ORlaptop .ORscreen {
+        height: 85px;
+        width: 130px;
+        border-radius: 8px;
+        transform: skew(18deg);
+        background: #f5f5f1;
+    }
+    .dog .ORlaptop .ORscreen::before {
+        display: block;
+        content: "";
+        height: 17px;
+        width: 10px;
+        position: relative;
+        top: 38px;
+        left: 50px;
+        border-radius: 6px;
+        background: #690ddc;
+    }
+    .dog .ORlaptop .ORscreen::after {
+        display: block;
+        content: "";
+        height: 17px;
+        width: 10px;
+        position: relative;
+        top: 21px;
+        left: 64px;
+        border-radius: 6px;
+        background: #690ddc;
+    }
+    .dog .ORlaptop .ORkeyboard {
+        height: 12px;
+        width: 132px;
+        position: relative;
+        left: 14px;
+        border-radius: 6px 0 0 6px;
+        background: #cbcbc7;
+    }
+    .dog .ORlaptop .ORkeyboard::before {
+        display: block;
+        content: "";
+        height: 12px;
+        width: 72px;
+        position: relative;
+        left: 128px;
+        border-radius: 6px;
+        background: #e5e5e1;
+    }
+    @keyframes catLeftType {
+        2% {
+            transform: translateY(-8px);
+        }
+        6% {
+            transform: none;
+        }
+        8% {
+            transform: translateY(-8px);
+        }
+        10% {
+            transform: none;
+        }
+        14% {
+            transform: translateY(-8px);
+        }
+        16% {
+            transform: none;
+        }
+        18% {
+            transform: translateY(-8px);
+        }
+        20% {
+            transform: none;
+        }
+        22% {
+            transform: translateY(-8px);
+        }
+        26% {
+            transform: none;
+        }
+    }
+    @keyframes catRightType {
+        6% {
+            transform: translateY(-8px);
+        }
+        8% {
+            transform: none;
+        }
+        10% {
+            transform: translateY(-8px);
+        }
+        12% {
+            transform: none;
+        }
+        16% {
+            transform: translateY(-8px);
+        }
+        18% {
+            transform: none;
+        }
+        20% {
+            transform: translateY(-8px);
+        }
+        22% {
+            transform: none;
+        }
+        24% {
+            transform: translateY(-8px);
+        }
+        28% {
+            transform: none;
+        }
+    }
+    @keyframes catRead {
+        55% {
+            transform: none;
+        }
+        62% {
+            transform: translateX(-2px);
+        }
+        70% {
+            transition-timing-function: ease-out;
+            transform: translateX(3px);
+        }
+        82% {
+            transform: translateX(-2px);
+        }
+        90% {
+            transition-timing-function: ease-out;
+            transform: translateX(3px);
+        }
+        100% {
+            transform: none;
+        }
+    }
+    @keyframes dogLeftType {
+        50% {
+            transform: none;
+        }
+        52% {
+            transform: translateY(-8px);
+        }
+        56% {
+            transform: none;
+        }
+        58% {
+            transform: translateY(-8px);
+        }
+        60% {
+            transform: none;
+        }
+        64% {
+            transform: translateY(-8px);
+        }
+        66% {
+            transform: none;
+        }
+        68% {
+            transform: translateY(-8px);
+        }
+        70% {
+            transform: none;
+        }
+        72% {
+            transform: translateY(-8px);
+        }
+        76% {
+            transform: none;
+        }
+    }
+    @keyframes dogRightType {
+        54% {
+            transform: none;
+        }
+        56% {
+            transform: translateY(-8px);
+        }
+        58% {
+            transform: none;
+        }
+        60% {
+            transform: translateY(-8px);
+        }
+        62% {
+            transform: none;
+        }
+        66% {
+            transform: translateY(-8px);
+        }
+        68% {
+            transform: none;
+        }
+        70% {
+            transform: translateY(-8px);
+        }
+        72% {
+            transform: none;
+        }
+        74% {
+            transform: translateY(-8px);
+        }
+        78% {
+            transform: none;
+        }
+    }
+    @keyframes dogRead {
+        5% {
+            transform: none;
+        }
+        17% {
+            transition-timing-function: ease-out;
+            transform: translateX(-5px);
+        }
+        25% {
+            transform: none;
+        }
+        37% {
+            transition-timing-function: ease-out;
+            transform: translateX(-5px);
+        }
+        45% {
+            transform: none;
+        }
+    }
+
+</style>
+
+
+<script>
+    export default{
+        name: 'ob-pets'
+    }
+</script>
---
+++ src/views/faq/faq.vue
@@ -0,0 +1,150 @@
+<template>
+    <div>
+        <section class="hero is-large has-border-bottom-gray">
+            <div class="hero-body">
+                <div class="container is-fluid">
+                    <div class="columns">
+                        <div class="column">
+                            <div class="columns is-centered padding-top-1x-mobile">
+                                <div class="column is-narrow">
+                                    <h1 class="subtitle is-size-3 has-text-grey-light has-text-centered-mobile">
+                                        FAQ
+                                    </h1>
+                                    <h2 class="title is-size-2 trust-text has-text-centered-mobile">
+                                        The frequently asked questions
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="columns is-centered is-vcentered contact-us-header-mailbox is-mobile">
+                                <div class="column is-narrow padding-top-1x-mobile">
+                                    <ob-pets/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section" id="faq">
+            <div class="small-container">
+                <div class="content is-medium">
+                    <h3>
+                        Why now?
+                    </h3>
+                    <p>
+                        After years of using and seeing existing platforms make questionable choices when it comes to the
+                        privacy
+                        and security of its users, we decided enough was enough.
+                    </p>
+                    <p>
+                        We had hoped someone else would take on the challenge. That someone, somewhere would realize how
+                        much of a difference this could make,
+                        not only in the context of social networks, but in the context of the security and privacy of the
+                        internet.
+                    </p>
+                    <p>
+                        There were several attempts, and some of them still exist to this day. Sadly sooner or later, they
+                        all gave up to
+                        pursue other markets like micro-communities and business solutions. Perhaps they thought going after
+                        the social network giants was too big of a challenge.
+                    </p>
+                    <p>
+                        Well, it sure is. Luckily, we believe the greatest accomplishments of mankind are often the ones
+                        that
+                        posed the greatest challenges.
+                    </p>
+                    <p>
+                        That being said, we are taking on the challenge.
+                    </p>
+                    <h3>
+                        How does Openbook differ from other established alternative social networks like Diaspora, Mastodon,
+                        Friendica, etc.?
+                    </h3>
+                    <p>
+                        Briefly put, we are a privacy and security first social network.
+                    </p>
+                    <p>
+                        Practically, this means we aim to have the functionality that MySpace, Facebook and other similar
+                        networks have, but designed to be secure and privacy aware right from the start.
+                    </p>
+                    <p>
+                        Some examples of this are
+                    </p>
+                    <ul>
+                        <li>
+                            No tracking, within or outside the network.
+                        </li>
+                        <li>
+                            No deep learning algorithms analyzing your personal information together with your usage of the
+                            platform to determine the content you are served (which are the source of <a href="https://en.wikipedia.org/wiki/Echo_chamber_(media)" target="_blank">echo chambers</a>).
+                        </li>
+                        <li>
+                            End to end encryption as the default way to communicate (even on the browser).
+                        </li>
+                        <li>
+                            Absolute control of your data (decentralised platforms cant do this). Our data sharing policy is is strict and transparent. You will always know where all your data is physically located and be able to delete it. Even with third party applications.
+                        </li>
+                    </ul>
+                    <p>
+                        Overall, other alternative social networks are too hard to use for normal people (like our grandpas and grandmas), try to fill in other market niches, or are making the same mistakes social media companies have made for the past few years in concern to the privacy and security of their users.
+                    </p>
+                    <h3>
+                        How will you make money?
+                    </h3>
+                    <p>
+                        Call us crazy but <strong>our business model is not and will never be advertisement</strong> (also
+                        known as
+                        your personal data).
+                    </p>
+                    <p>
+                        We will build Openbook to be a platform for peer-to-peer services such as
+                        marketplaces.
+                    </p>
+                    <p>
+                        For example, you could sell your beloved Star Wars collection on the network in the same way you
+                        could hire someone to walk your dog.
+                    </p>
+                    <p>
+                        Applications built within the platform will benefit from the privacy and security of the ecosystem.
+                    </p>
+                    <p>
+                        Apart from this, we are also looking into offering business services.
+                    </p>
+                    <p>
+                        We would offer them help setting up their own private internal Openbook networks with extra business
+                        features such as event planning, team work planning, payslips management and identity management.
+                    </p>
+                    <h3>
+                        What if you don't meet your funding goals?
+                    </h3>
+                    <p>
+                        If we don't meet our funding goals, we hope to serve as an inspiration for existing and upcoming
+                        social
+                        networks.
+                    </p>
+                    <p>
+                        We hope they take note of our ideas and implement them. We hope they put their users privacy and
+                        security
+                        first.
+                    </p>
+                    <p>
+                        And finally, we hope they realise new ways of making money can be thought of that don't involve
+                        monetizing the
+                        personal information of their users.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    import ObPets from "./components/pets";
+
+    export default{
+        components: {ObPets},
+        name: 'ob-faq'
+    }
+</script>
