dcfef52149f31e18b3d8b31366cf8f65a996aaa8
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-22 23:42:53 +0200

    🔥 remove language dropdown for now

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -6,18 +6,6 @@
                     <router-link to="home" class="navbar-item is-size-4 has-text-primary">
                         <strong>Open</strong>book.
                     </router-link>
-                    <div class="navbar-item is-hidden-desktop">
-                        <b-dropdown>
-                            <button class="button is-primary" slot="trigger">
-                                <span>English</span>
-                                <span class="icon">
-                                        <i class="fas fa-angle-down"></i>
-                                    </span>
-                            </button>
-
-                            <b-dropdown-item>Spanish</b-dropdown-item>
-                        </b-dropdown>
-                    </div>
                     <ob-hamburger></ob-hamburger>
                 </div>
                 <div class="navbar-menu">
@@ -28,18 +16,6 @@
                         <router-link to="contact-us" class="navbar-item" active-class="is-active">Contact us</router-link>
                         <router-link to="jobs" class="navbar-item" active-class="is-active">Jobs</router-link>
                         <router-link to="faq" class="navbar-item" active-class="is-active">FAQ</router-link>
-                    </div>
-                    <div class="navbar-item">
-                        <b-dropdown>
-                            <button class="button is-primary" slot="trigger">
-                                <span>English</span>
-                                <span class="icon">
-                                        <i class="fas fa-angle-down"></i>
-                                    </span>
-                            </button>
-
-                            <b-dropdown-item>Spanish</b-dropdown-item>
-                        </b-dropdown>
                     </div>
                 </div>
             </div>
