81b4b0b7a63e8330c912edfaaf6fcdc39fdee247
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-22 22:10:39 +0200

    ✨ add meet the team photos

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -27,40 +27,8 @@
         <section class="section">
             <ob-count-down></ob-count-down>
         </section>
-        <section class="hero is-primary is-bold">
-            <div class="hero-body has-text-centered">
-                <div class="container">
-                    <div class="columns is-centered is-vcentered">
-                        <div class="column">
-                            <a href="#" class="button is-white is-outlined is-rounded is-medium">
-                                Meet the team
-                            </a>
-                        </div>
-                        <div class="column">
-                            <div class="columns is-centered is-mobile">
-                                <div class="column is-narrow">
-                                    <div class="home-team">
-                                        <figure class="image is-64x64 home-team-figure">
-                                            <img class="is-rounded" src="https://preview.webpixels.io/boomerang-v3.6/assets/images/prv/people/person-3.jpg" alt="Image">
-                                        </figure>
-                                        <figure class="image is-64x64 home-team-figure">
-                                            <img class="is-rounded" src="https://preview.webpixels.io/boomerang-v3.6/assets/images/prv/people/person-3.jpg" alt="Image">
-                                        </figure>
-                                        <figure class="image is-64x64 home-team-figure">
-                                            <img class="is-rounded" src="https://preview.webpixels.io/boomerang-v3.6/assets/images/prv/people/person-3.jpg" alt="Image">
-                                        </figure>
-                                        <figure class="image is-64x64 home-team-figure">
-                                            <img class="is-rounded" src="https://preview.webpixels.io/boomerang-v3.6/assets/images/prv/people/person-3.jpg" alt="Image">
-                                        </figure>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section">
+        <ob-meet-the-team></ob-meet-the-team>
+<!--        <section class="section">
             <div class="container">
                 <div class="content has-text-centered padding-bottom-2x">
                     <p class="is-size-3 padding-bottom-3x">
@@ -69,7 +37,7 @@
                 </div>
                 <ob-quotes></ob-quotes>
             </div>
-        </section>
+        </section>-->
         <section class="section">
             <div class="container">
                 <div class="content has-text-centered padding-bottom-2x">
@@ -102,9 +70,11 @@
     import ObOcean from "./components/splash-hero/components/ocean/ocean";
     import ObSplashHero from "./components/splash-hero/splash-hero";
     import ObFeaturedOn from "./components/featured-in/featured-in";
+    import ObMeetTheTeam from "./components/meet-the-team/meet-the-team.vue";
 
     export default {
         components: {
+            ObMeetTheTeam,
             ObFeaturedOn,
             ObSplashHero,
             ObOcean,
