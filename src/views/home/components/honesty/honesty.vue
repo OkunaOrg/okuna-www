02089d678663041f17ce368d6e82a27b803a1d3e
Joel Hernandez <lifenautjoe@gmail.com>
2018-07-02 19:34:10 +0200

    ✨ add honesty section beating heart

--- src/views/home/components/honesty/honesty.vue
+++ src/views/home/components/honesty/honesty.vue
@@ -1,27 +1,28 @@
 <template>
-    <section class="section">
+    <section class="section honesty-section">
         <div class="container">
             <div class="columns">
+                <div class="column has-padding-top-2x-mobile">
+                    <div class="columns is-centered is-vcentered is-height-100 is-mobile">
+                        <div class="column is-narrow">
+                            <ob-heart></ob-heart>
+                        </div>
+                    </div>
+                </div>
                 <div class="column">
-                    <div class="columns is-centered is-vcentered">
-                        <div class="column is-narrow content is-medium" style="max-width: 400px">
-                            <h3>Honest</h3>
+                    <div class="columns is-vcentered is-mobile">
+                        <div class="column content is-medium honesty-text">
+                            <h3 class="has-text-centered-mobile">Honest</h3>
                             <p>
-                                All the code behind Openbook is open-source, meaning free for anyone to see, modify and reproduce.
+                                All the code behind Openbook is open-source, meaning free for anyone to see, modify and
+                                reproduce.
                             </p>
                             <p>
                                 No place for tricks, no place for lies, no unpleasant surprises.
                             </p>
                             <p>
-                                <strong>Transparency and honesty at its finest.</strong>
+                                <strong>Transparent and honest.</strong>
                             </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="columns is-centered is-vcentered is-mobile">
-                        <div class="column is-narrow">
-                            Test2
                         </div>
                     </div>
                 </div>
@@ -30,8 +31,27 @@
     </section>
 </template>
 
+<style lang="scss">
+    .honesty-section{
+        padding: 5rem;
+    }
+
+    .honesty-text {
+        @media (min-width: 768px) {
+            max-width: 400px;
+        }
+    }
+
+    .honesty-heart{
+
+    }
+</style>
+
 <script>
+    import ObHeart from "./components/heart";
+
     export default {
+        components: {ObHeart},
         name: 'ob-honesty'
     }
 </script>
