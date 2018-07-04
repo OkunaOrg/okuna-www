062e442057cd81a9b0d3a993edaee5573ad34f92
Shantanu <shantanu.tarey@kpn.com>
2018-07-04 21:15:38 +0200

    ✨ manifesto in 3 languages, 3 directories

---
+++ src/locale/es.js
@@ -0,0 +1,148 @@
+export const messages = {
+    en : {
+        global: {
+            openbook: '<strong>Open</strong>book'
+        },
+        home : {
+            want_to_know_more: 'Want to know more?',
+            read_manifesto: 'Read our manifesto',
+            featured_in: 'As featured in'
+        },
+        splash_hero: {
+            hello_world: 'Hello <span id="hello-subject">world!</span>',
+            world: 'world',
+            friends: 'friends',
+            family: 'family',
+            grandma: 'grandma',
+            reddit: 'reddit',
+            we_are_ob: 'We\'re <strong>Open</strong>book.',
+            kickstarter: 'An open source, zero tracking, zero spying, zero\n' +
+            '                                    ads, ethically designed, easy to migrate to,\n' +
+            '                                    social network <strong>soon on <span\n' +
+            '                                    style="color: #034752;">Kickstarter</span></strong>.',
+            share: 'Share',
+            notify_me: 'Notify me'
+        },
+        why_ob: {
+            title: 'Why Openbook?',
+            subtitle: 'Some of the key things that set us apart from other social networks.',
+            zero_tracking: 'Zero tracking',
+            zero_tracking_desc: 'We will never track you, anywhere, anyhow.',
+            zero_spying: 'Zero spying',
+            zero_spying_desc: 'Be confident your private conversations are truly private.',
+            zero_ads: 'Zero advertisement',
+            zero_ads_desc: 'Advertisement lead to targeted advertisement, which in turn lead to the loss of online privacy. We won\'t have ads.',
+            data_protection: 'Tough data protection',
+            data_protection_desc: 'Third party applications that wish to access your data will have to comply with our strict and tough security policies.',
+            honest: 'Honest',
+            honest_desc: 'All of our code is free and open for everyone to inspect, reproduce and contribute to. No unpleasant surprises.',
+            easy_to_migrate: 'Easy to migrate',
+            easy_to_migrate_desc: 'We will provide tools to migrate all of your existing data to our network.',
+            future_proof: 'Future-proof',
+            future_proof_desc: 'With a solid and useful revenue model based on peer-to-peer transactions, we are in for the long run.',
+            modern_design: 'Modern, beautiful, ethically designed.',
+            modern_design_desc: 'Built with the latest technologies, a keen eye for detail and ethics, you won\'t find anything like it elsewhere.',
+            good_for_planet: 'Good for the planet',
+            good_for_planet_desc: 'In partnership with <a href="https://founderspledge.com/" target="_blank"\n' +
+            '                                                             rel="noopener noreferrer">Founders Pledge</a>\n' +
+            '                                we will give 30% of our revenue towards making the world a better place, now and\n' +
+            '                                    forever.'
+        },
+        count_down: {
+            kickstarter: 'Kickstarter',
+            campaign_countdown: 'campaign countdown',
+            days: 'days',
+            hours: 'hours',
+            minutes: 'minutes',
+            seconds: 'seconds',
+            kickstarter_launch: 'We will launch our <strong class="has-text-kickstarter">Kickstarter</strong> ' +
+            'campaign <br/> on the <strong>15th of July of 2018</strong>.'
+        },
+        meet_the_team: {
+            title: 'Meet the team'
+        },
+        header: {
+            home: 'Home',
+            about_us: 'About Us',
+            manifesto: 'Manifesto',
+            contact_us: 'Contact Us',
+            jobs: 'Jobs',
+            faq: 'FAQ',
+            press: 'Press'
+        },
+        footer: {
+            report_vulnerability: 'Report Vulnerability',
+            twitter: 'Twitter',
+            github: 'Github',
+            reddit: 'Reddit',
+            gitter: 'Gitter',
+            linked_in: 'LinkedIn',
+            product_hunt: 'Product Hunt',
+            facebook: 'Facebook',
+            the_ob_organization: 'The Openbook Organization',
+            privacy_policy: 'Privacy Policy',
+            made_with: 'Made with',
+            in_europe: 'in Europe'
+        },
+        about_us: {
+            title: 'About us',
+            subtitle: 'We want to build a tool for <span class="has-text-primary">good</span>.',
+            description: '<span class="has-text-primary"><strong\n' +
+            '                                class="has-text-primary">Open</strong>book</span> ' +
+            'is an open-source, zero tracking, zero spying, zero advertisement, secure social network ' +
+            'that gives 30% of it\'s revenue towards making the world a better place, now and forever.',
+            description_founded: 'Founded in April 2018 in the international city of peace and justice, The Hague, Openbook is a project to make things right.',
+            description_project: 'A project to redefine social networks, a project to rethink the "free" internet, a project to bring transparency to the internet, a project to claim back our privacy.',
+            description_better_world: '<strong>Openbook is a project to make the world a better place</strong>.',
+            accomplish_how: 'How will we accomplish this?',
+            read_our_manifesto: 'Read our manifesto',
+            humans_behind_ob: 'The humans behind <strong>Open</strong>book',
+            work_with_us: 'Want to work with us?',
+            visit_jobs_page: 'Visit our jobs page',
+            our_ambassadors: ' Our ambassadors',
+            ambassadors_desc: 'People that help us spread the word around and stand for everything we do.',
+            become_ambassadors: 'Want to become an ambassador?',
+            contact_us: 'Contact us'
+        },
+        team_members: {
+            joel_title: 'Founder, Chief Executive Officer',
+            joel_desc: 'Privacy and security activist, software engineer, digital products designer, open sourcerer, The Next Web Top 500 young talent, human.',
+            jaya_title: 'Chief Operating Officer',
+            jaya_desc: 'Globally recognized cyber-security expert, speaker on lawful interception, quantum-computing, counter surveillance and cryptography.',
+            phil_title: 'Chief Cryptographer',
+            phil_desc: 'Cryptography pioneer, creator of PGP, the most widely used email encryption software in the world, internet hall of fame member, privacy activist.',
+            lambrecht_title: 'Chief Marketing Officer',
+            lambrecht_desc: 'Security and international relations expert, conflict analyst, peace-builder, media researcher & producer, human rights activist.',
+            vincent_title: 'Security Engineer',
+            vincent_desc: 'Ethical hacker, reverse-engineer, exploit developer, speaker, does not like the word cyber, cyber expert.',
+            bas_title:  'UI/UX Ethical Designer',
+            bas_desc: 'Advocate of useful and meaningful products, user experience wizard, worked with numerous international brands, researcher, hardware hacker.',
+            shantanu_title: 'Software Engineer',
+            shantanu_desc: 'Pragmatic software engineer, product guy, crypto-geek, bot-maker, simplicity advocate.',
+            martijn_title: 'Software Engineer',
+            martijn_desc: 'Practical software engineer, JavaScript guru, wearable tech geek, gamer, coolness officer in charge.'
+        },
+        contact_us: {
+            title: 'Contact us',
+            subtitle: 'We want to hear from you.',
+            info: 'Contact information',
+            message: 'Send us a message'
+        },
+        address: {
+            name: 'Name',
+            ob_organization: 'The Openbook Organization',
+            address: 'Address',
+            ob_address: 'Bagijnestraat 25, 2511CK, The Hague, The Netherlands',
+            telephone: 'Telephone',
+            telephone_no: '(+31) 611 60 4275',
+            email: 'Email',
+            website: 'Website'
+        },
+        message: {
+            subject: 'Subject',
+
+        }
+    }
+};
+
+
