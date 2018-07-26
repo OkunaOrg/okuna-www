d1124aafea70e6f4ff0bc1008797b7dc9bd45581
Joel Hernandez <lifenautjoe@gmail.com>
2018-07-26 02:06:56 +0200

    🔖 add latest dist

--- src/views/home/components/currency/currency.vue
+++ src/views/home/components/currency/currency.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="section">
         <div class="container">
-            <div class="columns">
+            <div class="columns has-reverse-row-order-mobile">
                 <div class="column has-padding-top-2x-mobile">
                     <div class="columns is-centered is-vcentered is-height-100 is-mobile">
                         <div class="column is-narrow">
