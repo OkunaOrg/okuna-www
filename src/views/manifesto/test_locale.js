81cff6e868e858b4de9e5cf7e88ba1ebd3661d6a
Shantanu <shantanu.tarey@kpn.com>
2018-07-27 20:11:04 +0200

    🍱 french and hungarian

---
+++ src/views/manifesto/test_locale.js
@@ -0,0 +1,276 @@
+{"hu": {
+"global": {
+"openbook": "<strong>Open<\/strong>book"
+},
+"home": {
+"want_to_know_more": "Szeretnél többet tudni?",
+"read_manifesto": "Olvasd el a kiáltványunkat",
+"featured_in": "Ők írtak már rólunk:"
+},
+"honesty": {
+"title": "Őszinte",
+"description": "Minden kód nyílt-forráskódú, vagyis ingyen megtekinthető, módosítható, illetve reprodukálható bárki által. Nincsenek váratlan meglepetések!"
+},
+"privacy": {
+"title": "Magánéletbarát",
+"description": "Nem figyeljük és nem követjük nyomon amit csinálsz. Szörfölj a hálózaton abszolút nyugalommal és magabiztossággal!"
+},
+"secure": {
+"title": "Biztonságos",
+"description": "Kemény és szigorú biztonsági irányelveinkkel biztosítani tudjuk, hogy az adataid sose kerülnek rossz kezekbe. Ráadásul egy kriptográfiai pionír is van a csapatunkban. End-to-end titkosítást, valaki?"
+},
+"easy": {
+"title": "Gyerekjáték",
+"description": "Könnyen használható, és még könnyebb beköltözni. Csak fogd és ejtsd le a régi közösségi oldalról származó archívumot az Openbook-ra és importáld fotóidat, videóidat, beszélgetéseidet és még sok mást!"
+},
+"fun": {
+"title": "Szórakoztató!",
+"description": "Visszahozzuk a közösséget a közösségi hálózatba, újból értelmet adunk a társaiddal való interakcióknak."
+},
+"personal": {
+"title": "Személyes",
+"description": "Szabd személyre a hálózatot, úgy ahogy akarod. Légy különb, légy önmagad!"
+},
+"good_for_planet": {
+"title": "Jó a bolygó számára",
+"description": "A <a href=\"https:\/\/founderspledge.com\/\" rel=\"noreferrer noopener\">FoundersPledge<\/a>-el való partnerségünkkel bevételünk 30%-át oktatás, fertőtlenítés, éghajlatváltozás-megelőzés, stb. projekteknek adományozzuk."
+},
+"ads_free": {
+"title": "100% reklámmentes",
+"description": "A bevételi modellünk soha nem lesz hirdetésekre alapozva. Élvezd a reklámmentes szörfözést!"
+},
+"quotes": {
+"joel_name": "Joel Hernández",
+"joel_title": "Az Openbook alapítója",
+"joel_quote": "Az Openbook nem csak a közösségi hálózatok evolúciós lépése, hanem egy világméretű humanitárius projekt is.",
+"vincent_name": "Vincent Ruijter",
+"vincent_title": "Etikus hacker",
+"vincent_quote": "A jelenlegi hálózatok napról napra egyre nagyobbak és nagyobbak, az Openbook pedig valószínűleg az utolsó esély valami különb, etikus és jobb megalkotására.",
+"phil_name": "Jaya Baloo",
+"phil_title": "Információ-biztonsági szakértő",
+"phil_quote": "Az Openbook egy jó megoldás, olyan társadalmi tér, ahol a felhasználói közösség szabadon megoszthatja adatait biztonságosan."
+},
+"splash_hero": {
+"hello_world": "Helló <span id=\"hello-subject\">világ!<\/span>",
+"world": "világ",
+"friends": "barátok",
+"family": "család",
+"grandma": "nagyi",
+"reddit": "reddit",
+"we_are_ob": "Mi vagyunk az <strong>Open<\/strong>book.",
+"kickstarter": "A nyílt forráskódú, magánéletbarát, szórakoztató és őszinte közösségi hálózat, mely segít a világ jobbá tételében.",
+"share": "Megosztás",
+"notify_me": "Támogass minket Kickstateren"
+},
+"why_ob": {
+"title": "Miért az Openbook?",
+"subtitle": "Néhány kulcsfontosságú dolog, amely elkülönít minket a többi közösségi hálózattól.",
+"zero_tracking": "Nulla követés",
+"zero_tracking_desc": "Soha nem fogunk követni, sehol, sehogyan.",
+"zero_spying": "Nulla kémkedés",
+"zero_spying_desc": "Ne aggódj, a privát beszélgetéseid valóban privátok lesznek.",
+"zero_ads": "Reklámmentes",
+"zero_ads_desc": "A hirdetések célzott hirdetésekhez vezetnek, ami viszont az online magánélet elvesztéséhez vezetnek. Nálunk nem lesznek reklámok.",
+"data_protection": "Kemény adatvédelem",
+"data_protection_desc": "Azon harmadik felektől származó alkalmazások, melyek hozzá szeretnének férni a személyes adataidhoz, meg kell feleljenek a szigorú és kemény biztonsági irányelveinknek.",
+"honest": "Őszinte",
+"honest_desc": "Az teljes forráskód ingyenes és nyilvánosan elérhető bárki számára a megvizsgáláshoz, reprodukáláshoz és hozzájáruláshoz. Nincs váratlan meglepetés.",
+"easy_to_migrate": "Könnyű áttérhetőség",
+"easy_to_migrate_desc": "Olyan eszközöket fogunk biztosítani, melyekkel minden adatodat áthozhatod hálózatunkra.",
+"future_proof": "Jövőbiztos",
+"future_proof_desc": "Peer-to-peer tranzakciókon alapuló szilárd és hasznos bevételi modellünkkel, hosszú távon működni fogunk.",
+"modern_design": "Modern, gyönyörű, etikailag tervezett",
+"modern_design_desc": "A legmodernebb technologiákra építve, valamint nagy odafigyeléssel a részletekre és etikákra, ilyesmit nem találsz máshol.",
+"good_for_planet": "Jó a bolygó számára",
+"good_for_planet_desc": "A <a href=\"https:\/\/founderspledge.com\/\" target=\"_blank\"\n                                                             rel=\"noopener noreferrer\">Founders Pledge-el<\/a>\n                                való partnerkapcsolatunkkal, bevételünk 30%-át jótékony célokra szenteljük, most és\n                                    mindörökké."
+},
+"count_down": {
+"kickstarter": "Kickstarter",
+"campaign_countdown": "kampány visszaszámlálás",
+"days": "nap | nap",
+"hours": "óra | óra",
+"minutes": "perc | perc",
+"seconds": "másodperc | másodperc",
+"kickstarter_launch": "A <strong class=\"has-text-kickstarter\">Kickstarter<\/strong> kampány véget ér <br\/> <strong>2018. augusztus 15<\/strong>-én."
+},
+"meet_the_team": {
+"title": "Ismerd meg a csapatot"
+},
+"header": {
+"home": "Főoldal",
+"about_us": "Rólunk",
+"manifesto": "Kiáltvány",
+"contact_us": "Kapcsolat",
+"jobs": "Karrier",
+"faq": "GYIK",
+"press": "Sajtó",
+"language": "Nyelv"
+},
+"footer": {
+"report_vulnerability": "Biztonsági rés jelentése",
+"twitter": "Twitter",
+"github": "Github",
+"reddit": "Reddit",
+"gitter": "Gitter",
+"linked_in": "LinkedIn",
+"product_hunt": "Product Hunt",
+"facebook": "Facebook",
+"the_ob_organization": "Az Openbook szervezet",
+"privacy_policy": "Adatvédelmi nyilatkozat",
+"made_with": "Készült",
+"in_europe": "-el Európában"
+},
+"about_us": {
+"title": "Rólunk",
+"subtitle": "Szeretnénk egy <span class=\"has-text-primary\">jóra törekvő<\/span> eszközt létrehozni.",
+"description": "Az <span class=\"has-text-primary\"><strong\n                                class=\"has-text-primary\">Open<\/strong>book<\/span> egy új, adatvédelem-központú, nyílt forráskódú és menő közösségi hálózat, mely bevételének 30%-át a világ jobbá tételére szenteli.",
+"description_founded": "Az Openbook egy olyan projekt, mely a béke és igazságosság nemzetközi városában, Hágában jött létre 2018 áprilisában és egy jobb közösségi hálózat megalkotását tűzi ki célul.",
+"description_project": "Egy közösségi hálózat, amely őszinte, tiszteletben tartja a felhasználók magánéletét, összehozza az embereket, öröm használni és jót tesz a bolygónak.",
+"accomplish_how": "Hogyan fogjuk ezt megvalósítani?",
+"read_our_manifesto": "Olvasd el a kiáltványunkat",
+"humans_behind_ob": "Az <strong>Open<\/strong>book emberei",
+"work_with_us": "Szeretnél velünk dolgozni?",
+"visit_jobs_page": "Tekintsd meg az állásainkat",
+"our_ambassadors": "Nagykövetek",
+"ambassadors_desc": "Emberek akik segítenek a hír terjesztésében és kiállnak mindenért amit mi teszünk.",
+"become_ambassadors": "Szeretnél nagykövet lenni?",
+"contact_us": "Lépj velünk kapcsolatba"
+},
+"team_members": {
+"joel_title": "Alapító, vezérigazgató",
+"joel_desc": "Adatvédelmi és biztonsági aktivista, szoftvermérnök, digitális termékek tervezője,The Next Web top 500 fiatal tehetség, ember.",
+"jaya_title": "Ügyvezető igazgató",
+"jaya_desc": "Globálisan elismert információ-biztonsági szakértő, beszélő olyan dolgokról, mint a törvényes lehallgatás, kvantum-számítástechnika, ellenfelügyelet és kriptográfia.",
+"phil_title": "Vezető kriptográfus",
+"phil_desc": "Kriptográfiai pionír, a világon a legtöbbet használt e-mail titkosítási szoftver - a PGP megalkotója, internet hall of fame tag, adatvédelmi aktivista.",
+"lambrecht_title": "Marketing vezérigazgató",
+"lambrecht_desc": "A biztonság és nemzetközi kapcsolatok szakértője, konfliktuselemző, béketeremtő, médiakutató és producer, emberjogi aktivista.",
+"vincent_title": "Biztonsági mérnök",
+"vincent_desc": "Etikus hacker, reverse-engineer, exploit fejlesztő, beszélő, nem szereti a cyber szót, cyber-szakértő.",
+"bas_title": "Tervezési vezető",
+"bas_desc": "A hasznos és értelmes termékek híve, UX mágus, számos nemzetközi brand-ekkel dolgozott, kutató, hardware hacker.",
+"shantanu_title": "Temékvezető",
+"shantanu_desc": "Pragmatikus szoftvermérnök, aki szeret nagyszerű termékeket és élményeket építeni, crypto-geek, bot-készítő, az egyszerűség híve.",
+"nathalie_title": "Projekt menedzser",
+"nathalie_desc": "Mester tervező, az optimizmus megszemélyesítője, szeretnéd készen látni a dolgokat? Hívd Nathalie-t.",
+"robin_title": "Kommunikációs igazgató",
+"robin_desc": "Tartalom-marketing bűvész, The Next Web top 500, szereti a strandokat, nem szereti a rutin felborulását, szereti a komfortzónán kívüli életet.",
+"martijn_title": "Szoftvermérnök",
+"martijn_desc": "Gyakorlati szoftvermérnök, JavaScript guru, hordható tech geek, gamer, menőségben levő tiszt."
+},
+"ambassadors": {
+"isaac_name": "Isaac Hernández",
+"isaac_desc": "Művész, szociális vállalkozó.",
+"ruben_name": "Ruben Van Vreeland",
+"ruben_desc": "A BitSensor számítógépes biztonsági szoftver vezérigazgatója és társalapítója.",
+"edwin_name": "Edwin van Andel",
+"edwin_desc": "A Zerocopter vezérigazgatója, nemzetközi beszélő, tapasztalt biztonsági szakértő."
+},
+"contact_us": {
+"title": "Lépj velünk kapcsolatba",
+"subtitle": "Szeretnénk hallani felőled.",
+"info": "Elérhetőségi adatok",
+"message": "Küldj nekünk egy üzenetet"
+},
+"address": {
+"name": "Név",
+"ob_organization": "Openbook B.V",
+"address": "Cím",
+"ob_address": "Bagijnestraat 25, 2511CK, Hága, Hollandia",
+"telephone": "Telefon",
+"telephone_no": "(+31) 611 60 4275",
+"email": "E-mail cím",
+"website": "Weboldal"
+},
+"message": {
+"subject": "Tárgy",
+"reason_placeholder": "A kapcsolatfelvétel oka",
+"email_placeholder": "Az e-mail cím, amelyen elérhetünk",
+"email": "E-mail cím",
+"user_message": "Üzenet",
+"user_message_placeholder": "Miben segíthetünk?",
+"google_recaptcha_title": "<a href=\"https:\/\/www.google.com\/recaptcha\/intro\/v3beta.html\" target=\"_blank\"\n                                              rel=\"noopener noreferrer\">Google Recaptcha<\/a>-t\n                                    használunk, az űrlappal való visszaélés megelőzéséhez.",
+"recaptcha_info": "A Google adatokat gyűjt rólad, hogy \"ellenőrizze\", hogy ember vagy-e.",
+"info_includes": "Ez az információ az alábbiakat tartalmazza:",
+"info_ip_location": "Az IP címed, mely felhasználható a tartózkodási hely meghatározásához, illetve az előzőleg meglátogatott webhelyekre való összevetésre.",
+"info_cookies": "A Google által az elmúlt 6 hónapban elhelyezett összes cookie.",
+"info_mouse_clicks": "Hány kattintást hajtottál végre (vagy koppintást, érintőképernyős eszköz esetén)",
+"info_css": "Az oldal CSS információja",
+"info_date": "A dátum",
+"info_browser_lang": "A böngésződ nyelve",
+"info_plugins": "A böngésződre telepített kiegészítők",
+"info_js": "Az oldalon található JavaScript objektumok",
+"recaptcha_authorization": "Engedélyezed, hogy betöltsük a Google Recaptchat és ezáltal megengedjük a Google-nak, hogy a fent említett információkat összegyűjtse?",
+"recaptcha_authorization_yes": "Igen, engedélyezem a Google Recaptcha betöltését",
+"or": "vagy",
+"recaptcha_authorization_no": "Nem, email küldése az info@open-book.org címre",
+"tick_box": "Kérünk, jelöld be ezt a négyzetet",
+"clear_form": "Űrlap törlése",
+"submit": "Küldés"
+},
+"jobs": {
+"title": "Karrier",
+"subtitle": "Dolgozz velünk.",
+"exciting_adventure": "Az egyik legizgalmasabb digitális kalandra hajózunk és tehetséges emberekre van szükségünk a fedélzetünkön!",
+"all_skills": "Mindenféle készséggel rendelkező embereket keresünk. Technológia, design, marketing, pénzügy, média, és még sorolhatnánk.",
+"need_your_help": "Ha bízol az <span class=\"has-text-primary\"><strong class=\"has-text-primary\">Open<\/strong>book<\/span>-ban és tehetséges vagy a munkádban, nagy örömmel várjuk segítségedet!",
+"send_cv": "Küldd el az önéletrajzodat a  <a\n                        href=\"mailto:jobs@open-book.org?subject=Job%20Application\">jobs@open-book.org<\/a> címre.",
+"look_forward": "Várjük, hogy találkozhassunk veled!",
+"colleagues": "Kivel fogsz dolgozni?",
+"meet_the_team": "Ismerd meg a csapatot"
+},
+"faq": {
+"title": "GYIK",
+"subtitle": "A gyakran ismételt kérdések.",
+"office_dog": "Az irodánk kutyája keményen dolgozik.",
+"jump_to": "Ugrás",
+"whos_paying": "Ki állta eddig a költségeket?",
+"is_distributed": "Az Openbook miért nem decentralizált?",
+"whos_paying_ans": "Eddig teljesen önmagunk álltuk a költségeket. Ez azonban nagyon nehéznek bizonyult.\n                        Mivel csapatunk legtöbb tagja egy teljes munkaidős állással rendelkezik az Openbook mellett, nagyon sok időbe telne, hogy kiadjuk az első verziót, és még utána is, nehezen tudnánk állni anak az IT infrastruktúrának a költségeit, mely szükséges ahhoz, hogy a már létező hálózatokkal versenyezzünk.",
+"crowd_funding": "Mivel azt szeretnénk, hogy a projekt ember- és nem tőkevezette legyen, úgy döntöttünk, hogy a közösségi finanszírozást választjuk.",
+"launching_kickstarter": "Kickstarter kampányunkat július 17-én indítjuk.",
+"why_not_non_profit": "Az Openbook miért nem non-profit?",
+"why_not_non_profit_ans": "Az Openbook for-profit szervezetté való alakítása nehéz döntés volt.",
+"check_all_boxes": "Bepipáljuk az összes non-profit jelölőnégyzetet és szeretjük a non-profit szervezeteket. Szeretjük a Wikipédiát, a Ghost Foundation-t, a Founders Pledge-t, Mozillát és még sok mást.",
+"same_struggle": "Azonban ugyanezt a küzdelmet újból és újból folyamatosan látjuk. Ezek a cégek nem tudnak növekedni a jövedelmezőségükön túl. Ezek a cégek nehezen tudnak megnőni méret és erőforrás szempontból olyannyira, hogy a for-profit cégekkel tudjanak versenyezni.",
+"need_money": "Amikor exponenciálisan kell növekednünk, szükségünk van arra, hogy szert tegyünk azokra a nagy összegekre, amelyek ehhez kellenek.",
+"hence_for_profit": "Ennek következtében mi hivatalosan egy for-profit cég vagyunk.",
+"humanitarian": "De ne feledd, hogy amikor ezt tesszük, biztosítani fogjuk, hogy az emberek akik a vállalatba akarnak befektetni olyan emberek, akik igazi érdeklődéssel néznek a platformunkra, valamint annak alapvető értékeire magánéleti, biztonsági, szabadsági, nyitottsági, valamint humanitárius természeti szempontból.",
+"want_to_help": "Szeretnék most segíteni, mit tehetek?",
+"want_to_help_ans": "Nagyon örülünk és köszönjük, hogy segíteni akarsz!",
+"many_ways": "Sok módon tudsz nekünk most segíteni.",
+"ideas": "Íme pár ötlet:",
+"share_website": "Oszd meg a weboldalunkat a közösségi hálózatokon",
+"talk_to_friends": "Beszélj rólunk a családi és baráti körödben",
+"improve_website": "Ha fejlesztő vagy, segíthetsz a <a\n                            href=\"https:\/\/github.com\/OpenBookOrg\/openbook-org-www\"\n                            target=\"_blank\" rel=\"noopener noreferrer\">weboldalunk jobbá tételében<\/a>.",
+"let_us_know": "Ha valamilyen ötleted van a hálózattal kapcsolatosan, <a href=\"https:\/\/github.com\/OpenBookOrg\/ideas\"\n                                                                            target=\"_blank\" rel=\"noopener noreferrer\">oszd meg velünk<\/a>.",
+"public_figure": "Ha egy híres személyiség vagy és nagykövet szeretnél lenni,",
+"contact_us": "lépj velünk kapcsolatba",
+"looking_for_job": "Ha munkát keresel és úgy gondolod, az Openbook-nál a helyed,",
+"visit_jobs": "tekintsd meg a karrierlistázó oldalunkat",
+"volunteer": "És végül, ha szeretnél még ennél is többet hozzájárulni projektünkhöz, szeretnénk megalkotni egy nemzetközi önkéntes-hálózatot, akik segítenek nekünk a hír elterjesztésében.",
+"interested": "Érdekel a dolog?",
+"contact_us_2": "Lépj velünk kapcsolatba",
+"more_questions": "Van egyéb kérdésed?",
+"send_us_message": "Küldj nekünk egy üzenetet",
+"why_not_decentralised": "Az Openbook miért nem decentralizált?",
+"why_not_decentralised_ans": "<p>\n  A decentralizált elképesztően bonyolultak. Például: frissítések kibocsátása (biztonsági frissítések\/fejlesztések beleértve), \n  verziók közötti kompatibilitás fenntartása (például egy olyan funkció kibocsátásakor, amely nem visszafele-kompatibilis, \n  tervezési szempontból) és nyomon követése, felhasználói információ módosítása vagy törlése csomópontokon keresztül (amelynek \n  működését lehetetlen garantálni).\n<\/p>\n\n<p>Egy hatalmas kihívás lenne megalkotni egy olyan rendszert, ami mindezeket a problémákat felülmúlja. Több évnyi kutatásba és fejlesztésbe kerülhet.<\/p>\n\n  <p>A szoftveres óriások hihetetlen sebességgel növkednek, eltiporva a konkurrencia összes nyomait.\n      A decentralizált közösségi hálózatokra törekvő kezdeményezések sajnos esély nélkül maradnak.<\/p>\n\n<p>Míg a decentralizálásra törekvő kezdeményezések arra összpontosítanak, hogy forradalmi technológiákat építsenek,\n  valamint megoldásokat a decentralizált rendszerek problémáira. Az ilyen rendszerek valódi felhasználói (technikai\n  háttér nélküli emberek) kevésbé törődnek a termék ilyen jellegű részleteivel. Ami őket érdekli az az élmény a funkciók,\n  a könnyen kezelhetőség, valamint a külalak. Amint ezek a kezdeményezések kifejlesztik a megfelelő megoldásokat,\n  fényévnyire lesznek a szoftveróriások mellett funkciók szempontjából. Minden a semmibe veszne.<\/p>\n\n<p>Mi fordítva közelítjük meg a problémát. Létező technológiákat használunk, hogy egy olyan terméket hozzunk létre, amely nemcsak szembe tud szállni a szoftveróriásokkal, de olyan szempontokból is legyőzi őket, ami a közönség érdekeit képviseli.<\/p>\n\n<p>Ha sikerül megalkotnunk a de facto közösségi hálózatot (mely máris egy nagy előrehaladás lenne\n  a már létezőek mellett transzparencia, adatvédelem és biztonság szempontjából), a megfelelő számú\n  felhasználóval és közérdekkel, meg fogjuk tudni időleg engedni magunknak, hogy a legjobbak legyünk a decentralizáció\n  stratégiáinak kutatásában.<\/p>\n\n<p>Képzeld el, milyen lenne ha milliókat tudnánk ebbe fektetni! Mekkora különbséget tenne.<\/p>"
+},
+"press": {
+"we": "Openbook",
+"press": "Sajtó",
+"today_everyday": "Ma és mindennap.",
+"hi": "Helló!",
+"any_questions": "Reméljük, hogy az alábbiakban megtalálsz mindent, amire szükséged van.\n                        ​​Ha kérdéseid vannak, be szeretnél ütemezni egy interjút vagy további anyagokra van szükséged, vedd fel a kapcsolatot Lambrecht Wessels-el a <a\n                        href=\"mailto:lambrecht@open-book.org?subject=Press%20inquiry&body=\">lambrecht@open-book.org<\/a> email címen.",
+"happy_to_help": "Nagy örömmel segítünk.",
+"jump_to": "Ugrás",
+"press_kit": "Sajtóanyagok",
+"newsroom": "Sajtószoba",
+"people": "Emberek",
+"logos": "Logók",
+"includes_everything": "Tartalmaz sajtóközleményeket, a nyilatkozatunkat, a történetünket, képeket, vektorlogókat, weboldal-, illetve egyéb képeket.",
+"home_press": "A sajtóközleményeink gyűjtőhelye.",
+"go_to_newsroom": "Ugrás a sajtószobába",
+"missing_something": "Hiányzik valami? Írj levelet <a\n                        href=\"mailto:lambrecht@open-book.org?subject=Press%20inquiry&body=\">Lambrecht Wessels<\/a>-nek."
+}
+}}
