bdd5f6378e23bbdb7eac44b365d16218abc4248f
Joel Hernandez <joel@open-book.org>
2019-01-10 13:01:47 +0100

    :sparkles: add personal image

---
+++ src/views/home/components/personal/components/personal-preview.vue
@@ -0,0 +1,24 @@
+<template>
+    <figure class="image">
+        <img class="personal-preview" :src="image" alt="Phone with personal features preview">
+    </figure>
+</template>
+
+<style lang="scss">
+    .personal-preview {
+    }
+</style>
+
+
+<script>
+    import image from './preview-black.png';
+
+    export default {
+        name: 'ob-personal-preview',
+        data() {
+            return {
+                image: image
+            }
+        }
+    }
+</script>
--- src/views/home/components/personal/personal.vue
+++ src/views/home/components/personal/personal.vue
@@ -4,9 +4,17 @@
             <div class="columns is-mobile is-multiline">
                 <div class="content is-medium column is-12 padding-bottom-3x">
                     <div class="columns">
-                        <div class="column is-6"></div>
                         <div class="column is-6">
-                            <div class="columns is-centered">
+                            <div class="column padding-top-4x-mobile">
+                                <div class="columns is-centered is-vcentered is-height-100 is-mobile">
+                                    <div class="column is-10">
+                                        <ob-personal-preview></ob-personal-preview>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column is-6">
+                            <div class="columns is-centered is-vcentered is-height-100">
                                 <div class="column is-narrow personal-text">
                                     <h3 class="has-text-shadow">{{ $t('personal.title') }}</h3>
                                     <p>
@@ -17,36 +25,28 @@
                         </div>
                     </div>
                 </div>
-                <div class="column is-12">
-                    <ob-customization-screenshots></ob-customization-screenshots>
-                </div>
             </div>
         </div>
     </section>
 </template>
 
 <style lang="scss">
-    .personal-section {
-        //padding: 5rem;
-    }
 
     .personal-text {
         @media (min-width: 768px) {
             max-width: 400px;
         }
     }
-
-    .personal-heart {
-
-    }
 </style>
 
 <script>
 
-    import ObCustomizationScreenshots from "./components/customization-screenshots/customization-screenshots.vue";
+    import obPersonalPreview from './components/personal-preview';
 
     export default {
-        components: {ObCustomizationScreenshots},
+        components: {
+            obPersonalPreview
+        },
         name: 'ob-personal'
     }
 </script>
