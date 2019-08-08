183256b917b434a03faf9610e431316af6b0d736
Emīls Gulbis <emils.gulbis@gmail.com>
2019-08-08 12:09:21 +0300

    @fullhuman/postcss-purgecss added

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,15 @@
+const autoprefixer = require("autoprefixer");
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+    plugins: [
+        autoprefixer(),
+        process.env.NODE_ENV === "production"? purgecss({
+            content: [
+                "./src/**/*.html",
+                "./src/**/*.vue",
+                "./src/**/*.js"
+            ]
+        }): ""
+    ]
 }
